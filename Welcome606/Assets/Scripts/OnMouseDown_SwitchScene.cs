6de0adda6676564b52b2337b2b36198a3534efcb
이지민 <145891038+leechimins@users.noreply.github.com>
2026-07-08 11:14:26 +0900

    ✨FEAT: 메인메뉴~맵01 연결 (#16) (#19)

---
+++ Welcome606/Assets/Scripts/OnMouseDown_SwitchScene.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class OnMouseDown_SwitchScene : MonoBehaviour
+{
+    public string sceneName;
+
+    void OnMouseDown()
+	{
+        SwitchScene();
+    }
+
+    public void SwitchScene()
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+}
