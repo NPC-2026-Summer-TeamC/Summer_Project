41deceef5ec631fede83f17ee0c3420810976058
scalarnvector <wings2248@gmail.com>
2026-07-24 19:48:09 +0900

    ✨FEAT: StageManager 기본 구조 구현 (#52)

---
+++ Welcome606/Assets/Scripts/Managers/StageManager.cs
@@ -0,0 +1,51 @@
+using UnityEngine;
+
+public class StageManager : MonoBehaviour
+{
+    [Header("Stage Info")]
+    [SerializeField] private int currentChapter;
+    [SerializeField] private int currentStage;
+
+    private bool isStageCleared;
+
+    private void Start()
+    {
+        StartStage();
+    }
+
+    // 스테이지 시작
+    private void StartStage()
+    {
+        isStageCleared = false;
+
+        // TODO : 오프닝 컷씬/대화 호출
+
+        StartPuzzle();
+    }
+
+    // 퍼즐 시작
+    private void StartPuzzle()
+    {
+        // TODO : 퍼즐 시작 처리
+        Debug.Log("Puzzle Start");
+    }
+
+    // 퍼즐 클리어 신호 수신
+    public void OnPuzzleClear()
+    {
+        if (isStageCleared)
+            return;
+
+        isStageCleared = true;
+
+        Debug.Log("Puzzle Clear");
+    }
+
+    private void ShowClearUI()
+    {
+    }
+
+    private void ReturnToMap()
+    {
+    }
+}
