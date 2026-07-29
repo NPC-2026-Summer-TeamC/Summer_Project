d5575718c27597ec58c61860547d191a64469c59
scalarnvector <wings2248@gmail.com>
2026-07-29 23:40:53 +0900

    ✨FEAT: SoundManager 싱글톤 구조 및 AudioSource 분리 구현 (#61)

---
+++ Welcome606/Assets/Scripts/Sound/SoundManager.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+    public static SoundManager Instance { get; private set; }
+
+    private AudioSource bgmSource;
+    private AudioSource sfxSource;
+
+    private void Awake()
+    {
+        if (Instance != null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        Instance = this;
+        DontDestroyOnLoad(gameObject);
+
+        bgmSource = gameObject.AddComponent<AudioSource>();
+        bgmSource.loop = true;
+        bgmSource.playOnAwake = false;
+
+        sfxSource = gameObject.AddComponent<AudioSource>();
+        sfxSource.loop = false;
+        sfxSource.playOnAwake = false;
+    }
+}
