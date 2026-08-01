cecd6be5bb1abd17fb3e31dc4988b35a6e7abc7d
scalarnvector <wings2248@gmail.com>
2026-08-01 22:10:55 +0900

    ✨FEAT: 드래그 시작 및 종료 처리 구현 (#73)

--- Welcome606/Assets/Scripts/Puzzle/DragInputManager.cs
+++ Welcome606/Assets/Scripts/Puzzle/DragInputManager.cs
@@ -9,4 +9,23 @@ public class DragInputManager : MonoBehaviour
     private List<TileData> dragTileList = new();
 
     private string currentColor;
+
+    // 드래그 시작
+    public void StartDrag()
+    {
+        isDragging = true;
+        dragTileList.Clear();
+
+        // TODO : Bag Randomizer를 통해 현재 색상 결정
+    }
+
+    // 드래그 종료
+    public void EndDrag()
+    {
+        isDragging = false;
+
+        // TODO : dragTileList의 타일 색상 및 RuntimeState 갱신
+
+        dragTileList.Clear();
+    }
 }
