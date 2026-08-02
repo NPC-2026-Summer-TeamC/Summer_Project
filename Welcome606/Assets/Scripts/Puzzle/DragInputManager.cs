c4008e14ca8222fbea51fc51a886d49ba757a915
scalarnvector <wings2248@gmail.com>
2026-08-02 11:50:26 +0900

    🔧REFACTOR: DragInputManager 구조 개선 (#73)

--- Welcome606/Assets/Scripts/Puzzle/DragInputManager.cs
+++ Welcome606/Assets/Scripts/Puzzle/DragInputManager.cs
@@ -9,7 +9,14 @@ public class DragInputManager : MonoBehaviour
     private bool isDragging;
     private List<TileData> dragTileList = new();
 
+    // TODO : BagRandomizer 구현 시 TileColor enum으로 변경
     private string currentColor;
+    private Camera mainCamera;
+
+    private void Awake()
+    {
+        mainCamera = Camera.main;
+    }
 
     // 드래그 시작
     private void StartDrag()
@@ -25,16 +32,12 @@ public class DragInputManager : MonoBehaviour
     {
         isDragging = false;
 
-        // TODO : BagRandomizer에서 현재 색상 가져오기
-
-        foreach (TileData tile in dragTileList)
-        {
-            // TODO : RuntimeState 갱신
-        }
+        ApplyColor();
 
         dragTileList.Clear();
     }
 
+    // 선택된 타일에 현재 색상 적용
     private void ApplyColor()
     {
         // TODO : BagRandomizer에서 현재 색상 가져오기
@@ -68,10 +71,12 @@ public class DragInputManager : MonoBehaviour
     private void UpdateDrag()
     {
         // 마우스 위치를 월드 좌표로 변환
-        Vector2 worldPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector2 worldPosition =
+            mainCamera.ScreenToWorldPoint(Input.mousePosition);
 
         // 현재 마우스 위치의 Tile 탐색
-        RaycastHit2D hit = Physics2D.Raycast(worldPosition, Vector2.zero);
+        RaycastHit2D hit =
+            Physics2D.Raycast(worldPosition, Vector2.zero);
 
         if (!hit)
         {
