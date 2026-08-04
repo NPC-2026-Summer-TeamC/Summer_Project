1570e0a08da530ee5ea2962a28e93b1808aa02f6
scalarnvector <wings2248@gmail.com>
2026-08-04 12:27:37 +0900

    ✨FEAT: Flood Fill 기반 Shape 탐색 구현 (#85)

--- Welcome606/Assets/Scripts/Puzzle/ShapeFinder.cs
+++ Welcome606/Assets/Scripts/Puzzle/ShapeFinder.cs
@@ -2,8 +2,16 @@ using System.Collections.Generic;
 
 public class ShapeFinder
 {
-    private readonly BoardManager boardManager;
+    private static readonly Coordinate[] Directions =
+    {
+        new(0, -1),
+        new(1, 0),
+        new(0, 1),
+        new(-1, 0)
+    };
 
+    private readonly BoardManager boardManager;
+    private bool[,] visited;
     private int nextShapeId;
 
     public ShapeFinder(BoardManager boardManager)
@@ -18,7 +26,30 @@ public class ShapeFinder
 
         int boardSize = boardManager.GetBoardSize();
 
-        // TODO : 보드 전체 탐색
+        visited = new bool[boardSize, boardSize];
+
+        for (int y = 0; y < boardSize; y++)
+        {
+            for (int x = 0; x < boardSize; x++)
+            {
+                if (visited[x, y])
+                {
+                    continue;
+                }
+
+                RuntimeState runtimeState =
+                    boardManager.GetRuntimeState(x, y);
+
+                if (!runtimeState.isColored)
+                {
+                    continue;
+                }
+
+                Shape shape = FindShape(x, y);
+
+                shapes.Add(shape);
+            }
+        }
 
         return shapes;
     }
@@ -26,11 +57,58 @@ public class ShapeFinder
     // 하나의 Shape 탐색
     private Shape FindShape(int startX, int startY)
     {
-        RuntimeState runtimeState =
+        RuntimeState startRuntimeState =
             boardManager.GetRuntimeState(startX, startY);
 
-        // TODO : Flood Fill(BFS) 구현
+        Shape shape =
+            new(nextShapeId++, startRuntimeState.color);
 
-        return new Shape(nextShapeId++, runtimeState.color);
+        Queue<Coordinate> queue = new();
+
+        queue.Enqueue(new Coordinate(startX, startY));
+
+        visited[startX, startY] = true;
+
+        while (queue.Count > 0)
+        {
+            Coordinate current = queue.Dequeue();
+
+            shape.tiles.Add(boardManager.GetTile(current.x, current.y));
+
+            foreach (Coordinate direction in Directions)
+            {
+                int nextX = current.x + direction.x;
+                int nextY = current.y + direction.y;
+
+                if (!boardManager.IsValidCoordinate(nextX, nextY))
+                {
+                    continue;
+                }
+
+                if (visited[nextX, nextY])
+                {
+                    continue;
+                }
+
+                RuntimeState nextRuntimeState =
+                    boardManager.GetRuntimeState(nextX, nextY);
+
+                if (!nextRuntimeState.isColored)
+                {
+                    continue;
+                }
+
+                if (nextRuntimeState.color != shape.color)
+                {
+                    continue;
+                }
+
+                visited[nextX, nextY] = true;
+
+                queue.Enqueue(new Coordinate(nextX, nextY));
+            }
+        }
+
+        return shape;
     }
 }
