d85600a55d910e5ed4a3e05d36d8e3afaad41743
scalarnvector <wings2248@gmail.com>
2026-08-04 16:03:52 +0900

    🔧REFACTOR: ShapeFinder 방문 처리 개선 (#85)

--- Welcome606/Assets/Scripts/Puzzle/ShapeFinder.cs
+++ Welcome606/Assets/Scripts/Puzzle/ShapeFinder.cs
@@ -55,6 +55,7 @@ public class ShapeFinder
 
                 if (!runtimeState.isColored)
                 {
+                    visited[x, y] = true;
                     continue;
                 }
 
